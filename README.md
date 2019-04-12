47826915662d833ee33d1866c847d508c0d1ec9b
Phil Hauser <phil@zweck.co>
2019-04-12 15:24:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://ccd-school.de/en/coding-dojo/classes-katas/ordered-jobs/
---
+++ index.js
@@ -0,0 +1,45 @@
+const R = require('ramda')
+
+const jobsConfig = `
+  b => d
+  a => b
+  c => a
+`
+// result: c -> a -> b -> d
+
+const parseJobs = R.pipe(
+  R.split('\n'),
+  R.filter(R.identity),
+  R.map(R.trim),
+  R.map(R.split(' => '))
+)
+
+const mapDeps = R.reduce((sorted, depPair) => {
+  const dependsOn = R.head(depPair)
+  const dependedOn = R.last(depPair)
+  const dependsOnIndex = R.indexOf(dependsOn, sorted)
+  const dependedOnIndex = R.indexOf(dependedOn, sorted)
+  const alreadyDependsOn = R.gte(dependsOnIndex, 0)
+  const alreadyDependedOn = R.gte(dependedOnIndex, 0)
+  const isCircular = alreadyDependedOn && R.gt(dependsOnIndex, dependedOnIndex)
+  const isEmpty = R.compose(R.not, R.length)
+
+  if (isCircular) {
+    throw new Error(`Circular dependency at ${depPair}`)
+  }
+
+  if (isEmpty(sorted)) return depPair
+
+  if (R.both(alreadyDependedOn, !alreadyDependsOn)) {
+    return [ dependsOn, ...sorted ]
+  }
+
+  return [ ...sorted, dependedOn ]
+}, [])
+
+const main = () => {
+  const jobs = parseJobs(jobsConfig)
+  const deps = mapDeps(jobs)
+  console.log(R.reverse(deps))
+}
+main()
